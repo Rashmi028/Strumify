134419f5bd127a787e25c9ef72bee2319c7924fc
Rashmi Verma <vermarashmi028@gmail.com>
2024-07-22 20:09:07 +0530

    commit

--- src/Listing/Sidebar.jsx
+++ src/Listing/Sidebar.jsx
@@ -3,56 +3,54 @@ import './sidebar.css';
 import { Slider } from '@mui/material';
 
 const Sidebar = () => {
-
   return (
     <>
-    <div className='sidebar'>
-      <div className='Categories'>
-        <h2>Categories</h2>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
+      <div className='sidebar'>
+        <div className='Categories'>
+          <h2>Categories</h2>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Electric Guitar</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Bass Guitar</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>ClassicalGuitar</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Keyboard</label>
+          </div>
+          <div className='more'>
+            +100 more
+          </div>
         </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
+        <hr/>
+        <div className='Brand'>
+          <h2>Brand</h2>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Taylor</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Fender</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Kawai</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Yamaha</label>
+          </div>
+          <div className='more'>
+            +100 more
+          </div>
         </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
-        </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
-        </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Acoustic Guitar</label>
-        </div>
-        <div className='more'>
-          +100 more
-        </div>
-        
-      </div>
-      <hr/>
-      <div className='Brand'>
-        <h2>Brand</h2>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Taylor</label>
-        </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Taylor</label>
-        </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Taylor</label>
-        </div>
-        <div className='category-menu'>
-          <input id="checkbox" type="checkbox" /><label>Taylor</label>
-        </div>
-        <div className='more'>
-          +100 more
-        </div>
-      </div>
-      <hr/>
-      <div className='Price'>
-        <h2>Price</h2>
-        <div className='Slider'>
-        <Slider
+        <hr/>
+        <div className='Price'>
+          <h2>Price</h2>
+          <div className='Slider' style={{ width: 200 }}>
+            <Slider
               aria-label="Price range"
               value={[0, 30]}
               onChange={(e, newValue) => console.log(newValue)}
@@ -69,28 +67,28 @@ const Sidebar = () => {
                 },
               }}
             />
-      </div>
-      <hr/>
-    </div>
-    <div className='Color'>
-      <h2>Color</h2>
-      <div className='category-menu'>
-      <input id="checkbox" type="checkbox" /><label>Red</label>
-      </div>
-      <div className='category-menu'>
-      <input id="checkbox" type="checkbox" /><label>Purple</label>
-      </div>
-      <div className='category-menu'>
-      <input id="checkbox" type="checkbox" /><label>Pink</label>
-      </div>
-      <div className='category-menu'>
-      <input id="checkbox" type="checkbox" /><label>Green</label>
-      </div>
-      <div className='more'>
-          +100 more
+          </div>
+     
         </div>
-    </div>
-    </div>
+        <div className='Color'>
+          <h2>Color</h2>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Red</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Purple</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Pink</label>
+          </div>
+          <div className='category-menu'>
+            <input id="checkbox" type="checkbox" /><label>Green</label>
+          </div>
+          <div className='more'>
+            +100 more
+          </div>
+        </div>
+      </div>
     </>
   );
 };
